1abdca6f289b131d02645b1b032f346536ba84ac
Ren-Logronio <reinhart.logronio@msugensan.edu.ph>
2024-02-01 01:33:43 +0800

    feat: routing

--- api/src/routes/listRouter.ts
+++ api/src/routes/listRouter.ts
@@ -1,26 +1,95 @@
 import express, { Request, Response } from "express";
-import { userModel as User } from "../models/userModel";
-import { ObjectId } from "mongodb";
-
-/*
-
-GET
-/user/ => all user
-/user/{id} => get user with id
-
-POST
-/user/ => post new user
-
-PUT
-/user/{id} => put user with id
-
-DELETE
-/user/{id} => delete user with id
-
-*/
+import { todoListModel as TodoList } from "../models/todoListModel";
+import { todoModel as Todo } from "../models/todoModel";
 
 const router = express.Router();
 
 // Place routes
+router.get('/', (res: Response, req: Request) => {
+    TodoList.find({}).then(
+        (todoList: any) => res.status(200).json(todoList),
+    ).catch(
+        (err: any) => res.status(500).json(err),
+    )
+})
+
+router.get('/:id', (res: Response, req: Request) => {
+    const todoListId = req.params.id;
+    TodoList.findById(req.params.id).then(
+        (todoList: any) => res.status(200).json(todoList),
+    ).catch(
+        (err: any) => res.status(500).json(err),
+    )
+})
+
+router.post('/', (res: Response, req: Request) => {
+    const newTodoList = new TodoList({ ...req.body });
+    newTodoList.save().then(
+        (todoList: any) => res.status(200).json(todoList),
+    ).catch(
+        (err: any) => res.status(500).json(err),
+    )
+});
+
+router.post('/:id/todo/', (res: Response, req: Request) => {
+    TodoList.findById(req.params.id).then(
+        (todoList: any) => {
+            const newTodo = new Todo({ ...req.body });
+            newTodo.save().then(
+                (todo: any) => {
+                    todoList.todos.push({ _id: todo._id });
+                    todoList.save().then(
+                        (todoList: any) => res.status(200).json(todoList),
+                    ).catch(
+                        (err:any) => { throw err },
+                    );
+                },
+            ).catch(
+                (err:any) => { throw err },
+            );
+        },
+    ).catch(
+        (err: any) => {
+            res.status(500).json(err);
+        },
+    );
+});
+
+router.put('/:id', (res: Response, req: Request) => {
+    const todoListId = req.params.id;
+    TodoList.findByIdAndUpdate(todoListId, { ...req.body }, { new: true }).then(
+        (todoList: any) => res.status(200).json(todoList),
+    ).catch(
+        (err: any) => res.status(500).json(err),
+    )
+});
+
+router.delete('/:id', (res: Response, req: Request) => {
+    const todoListId = req.params.id;
+    TodoList.findByIdAndDelete(todoListId).then(
+        (todoList: any) => res.status(200).json(todoList),
+    ).catch(
+        (err: any) => res.status(500).json(err),
+    )
+})
 
 export default router;
+
+/*
+7 routes
+
+GET *
+/list/ => all list *
+/list/{id} => get list with id *
+
+POST *
+/list/ => post new list *
+/list/{listId}/todo/ => post new todo to list *
+
+PUT *
+/list/{id} => put list with id *
+
+DELETE *
+/list/{id} => delete list with id *
+
+*/
--- api/src/routes/todoRouter.ts
+++ api/src/routes/todoRouter.ts
@@ -1,54 +1,85 @@
 import express, { Request, Response } from "express";
 import { todoModel as Todo } from "../models/todoModel";
 import { ObjectId } from "mongodb";
-import { Error } from "mongoose";
-
-/*
-
-GET
-/todo/ => all todo
-/todo/{id} => get todo with id
-
-POST
-/todo/ => post new todo
-
-PUT
-/todo/{id} => put todo with id
-
-DELETE
-/todo/{id} => delete todo with id
-
-*/
 
 const router = express.Router();
 
 router.get("/", (req: Request, res: Response) => {
     Todo.find({}).then(
         (todo: any) => { 
-            res.sendStatus(200);
-            res.send(todo);
+            res.status(200).json(todo);
         }
     ).catch(
-        (err: Error) => {
-            res.sendStatus(500);
-            res.send(err);
+        (err: any) => {
+            res.status(500).json(err);
         }
     );
 });
 
 router.get("/:id", (req: Request, res: Response) => {
     const todoId = req.params.id;
-    Todo.find({ _id: new ObjectId(todoId) }).then(
+    Todo.findById(todoId).then(
         (todo: any) => {
-            res.sendStatus(200);
-            res.send(todo);
+            res.status(200).json(todo);
         }
     ).catch(
-        (err: Error) => {
-            res.sendStatus(500);
-            res.send(err)
+        (err: any) => {
+            res.status(500).json(err)
         }
     )
 });
 
+router.post("/", (req: Request, res: Response) => {
+    const newTodo = new Todo({ ...req.body, completion: false });
+    newTodo.save().then(
+        (todo: any) => {
+            res.status(200).json(newTodo);
+        }
+    ).catch(
+        (err: any) => {
+            res.status(500).json(err)
+        }
+    )
+});
+
+router.put("/:id", (req: Request, res: Response) => {
+    const todoId = req.params.id;
+    Todo.findByIdAndUpdate(todoId, { ...req.body }, { new: true }).then(
+        (todo: any) => {
+            res.status(200).json(todo);
+        }
+    ).catch(
+        (err: any) => {
+            res.status(500).json(err);
+        }
+    )
+});
+
+router.delete("/:id", (req: Request, res: Response) => {
+    const todoId = req.params.id;
+    Todo.findByIdAndDelete(todoId).then(
+        (todo: any) => res.status(200).json(todo)
+    ).catch(
+        (err: any) => res.status(500).json(err)
+    )
+});
+
 export default router;
+
+/*
+5 routes
+
+GET *
+/todo/ => all todo *
+/todo/{id} => get todo with id *
+
+POST *
+/todo/ => post new todo *
+
+PUT *
+/todo/{id} => put todo with id *
+
+DELETE *
+/todo/{id} => delete todo with id *
+
+*/

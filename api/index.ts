013d77641c819456452d6c94138c02063f3157b4
Ren-Logronio <reinhart.logronio@msugensan.edu.ph>
2024-01-31 15:28:27 +0800

    chore: moved mongoose db

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,18 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
-import { database } from "./src/app/database";
-import { auth } from "./src/app/auth"; 
+import mongoose from "mongoose";
 
 const port = process.env.PORT || 3000;
+const mongodb_host = process.env.MONGODB_HOST || 'localhost'
+const mongodb_port = process.env.MONGODB_PORT || 27017
+const mongodb_db = process.env.MONGODB_DB || 'mern-todolist-db'
+
+mongoose.connect(`mongodb://${mongodb_host}:${mongodb_port}/${mongodb_db}`).then(() => {
+  console.log(`Mongoose connected @ mongodb://${mongodb_host}:${mongodb_port}/${mongodb_db}`);
+}).catch((err: Error) => {
+  console.log(`Mongoose connection error: ${err.message}`);
+});
 
 express()
   .use(cors({ origin: true }))
--- api/src/app/database.ts
+++
@@ -1,5 +0,0 @@
-import mongoose from 'mongoose';
-
-const database = mongoose.connect("mongodb://localhost:27017/mern-todolist-db");
-
-export { database };

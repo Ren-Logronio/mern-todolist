70f1cb7d96b8950e8421fed33c07217da8f61377
Ren-Logronio <reinhart.logronio@msugensan.edu.ph>
2024-02-03 05:43:20 +0800

    chore: implement shadcn

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,10 @@
-import Footer from './components/Footer'
-import Header from './components/Header'
-import Dash from './pages/Dash'
-import AppContainer from './components/AppContainer'
+
 
 function App() {
 
   return (
-    <AppContainer>
-      <Header/>
-      <Dash></Dash>
-      <Footer/>
-    </AppContainer>
+    <>
+    </>
   )
 }
 
--- client/src/components/Footer.tsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-    return (
-        <div className="flex flex-row justify-center border-t border-solid border-slate-200">
-            <u>Mama Mern Todo list</u>
-            <p className=" m-0 ml-2">by Reinhart F. Logronio</p>
-        </div>
-    )
-}

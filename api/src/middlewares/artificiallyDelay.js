3f7ebf3403739f63bf4033706abee39efcfe4e9c
Ren-Logronio <reinhart.logronio@msugensan.edu.ph>
2024-02-05 10:18:35 +0800

    Revert "chore: build dist"
    This reverts commit 781bd7f75c57f377b9aae6eb7c6d9fbc96aa0c50.

--- api/src/middlewares/artificiallyDelay.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function default_1(req, res, next) {
-    setTimeout(() => { next(); }, 200);
-}
-exports.default = default_1;

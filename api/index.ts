826b5ee6d03b03bf8c94080d3e5c2dd55693e07c
Ren-Logronio <reinhart.logronio@msugensan.edu.ph>
2024-02-06 23:14:52 +0800

    chore: rm env

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 import express from "express"
 import cors from "cors"
@@ -6,18 +9,16 @@ import todoRouter from "./src/routes/todoRouter"
 import listRouter from "./src/routes/listRouter"
 import authRouter from "./src/routes/authRouter"
 const port = process.env.PORT || 3000;
-const mongodb_host = process.env.MONGODB_HOST || 'localhost'
-const mongodb_port = process.env.MONGODB_PORT || 27017
-const mongodb_db = process.env.MONGODB_DB || 'mern-todolist-db'
-import dotenv from 'dotenv';
 
-dotenv.config();
 
-mongoose.connect(`mongodb://${mongodb_host}:${mongodb_port}/${mongodb_db}`).then(() => { 
-  console.log(`Mongoose connected @ mongodb://${mongodb_host}:${mongodb_port}/${mongodb_db}`);
-}).catch((err: Error) => {
-  console.log(`Mongoose connection error: ${err.message}`);
-});
+// const mongodb_host = process.env.MONGODB_HOST || 'localhost'
+// const mongodb_port = process.env.MONGODB_PORT || 27017
+// const mongodb_db = process.env.MONGODB_DB || 'mern-todolist-db'
+// mongoose.connect(`mongodb://${mongodb_host}:${mongodb_port}/${mongodb_db}`).then(() => { 
+//   console.log(`Mongoose connected @ mongodb://${mongodb_host}:${mongodb_port}/${mongodb_db}`);
+// }).catch((err: Error) => {
+//   console.log(`Mongoose connection error: ${err.message}`);
+// });
 
 // mongoose.connect(`mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASS}@${process.env.ATLAS_CLUSTER}/${process.env.ATLAS_DB}`).then(() => { 
 //   console.log(`Mongoose connected @ mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASS}@${process.env.ATLAS_CLUSTER}/${process.env.ATLAS_DB}`);
--- client/src/components/ui/footer.tsx
+++ client/src/components/ui/footer.tsx
@@ -2,7 +2,7 @@ import { HoverCard, HoverCardTrigger, HoverCardContent } from "./hover-card";
 
 
 
-export default function Footer({ className }: {className: string}) {
+export default function Footer({ className }: {className?: string}) {
     return (
       <div className={` flex-row items-center justify-center border-t hidden md:flex border-gray-300 bg-white ${ className ? className : ""}`}>
         <div className=" flex flex-row items-center justify-between mx-8 w-screen sm:min-w-0 md:min-w-[640px] max-w-[896px]">

dc43b910c1bc446d083aba93df887dd34db77f76
Ren-Logronio <reinhart.logronio@msugensan.edu.ph>
2024-02-07 11:19:30 +0800

    ..

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
       '/api': {
         target: "https://mern-todolist-api.vercel.app",
         changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
